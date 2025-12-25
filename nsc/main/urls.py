0c8d26201e086e8d41dca791174890b49ab00510
ayousocrazy <ayousocrazy@gmail.com>
2025-12-25 16:39:30 +0545

    Add nav-bar

--- nsc/main/urls.py
+++ nsc/main/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('academics/', views.academics, name="academics"),
+    path('admissions/', views.admissions, name="admissions"),
+    path('alumini/', views.alumini, name="alumini"),
+    path('contact/', views.contact, name="contact"),
+    path('events/', views.events, name="events"),
+    path('faqs/', views.faqs, name="faqs"),
 ]
--- nsc/main/views.py
+++ nsc/main/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "main/home.html")
+
+def academics(request):
+    return render(request, "main/academics.html")
+
+def admissions(request):
+    return render(request, "main/admissions.html")
+
+def alumini(request):
+    return render(request, "main/alumini.html")
+
+def contact(request):
+    return render(request, "main/contact.html")
+
+def events(request):
+    return render(request, "main/events.html")
+
+def faqs(request):
+    return render(request, "main/faqs.html")
